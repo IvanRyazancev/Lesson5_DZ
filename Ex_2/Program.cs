fef29e2471b529e50648bb16bba681a237800bf4
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-01-31 01:29:27 +0300

    Задача №2. Показать числа Фибоначчи. Задача решена.

---
+++ Ex_2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача №2. Показать числа Фибоначчи.
+
+int Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+Console.Write("Введите кол-во элементов последовательности Фибоначчи: ");
+int length = int.Parse(Console.ReadLine()!);
+
+for (int i = 1; i <= length; i++)
+{
+
+    Console.Write(Fibonacci(i));
+}
